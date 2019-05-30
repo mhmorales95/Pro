1b8f0feee07899562ad29e9ab7cdbbc4b4e26205
mhmorales95 <34465106+mhmorales95@users.noreply.github.com>
2019-05-29 20:10:16 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/proyecto1/MainActivity.java
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     int menor = 100;
-    //HOLA
+    //HOLAPPPHOLA
     long tiempoI = System.currentTimeMillis();
     ArrayList<Button> botones = new ArrayList<>();
     @Override
